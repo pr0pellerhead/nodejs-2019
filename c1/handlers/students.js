50169ada4de64df96824b6b4ef42d9b5b7449f33
Bojan Gavrovski <bojang@gmail.com>
2019-03-01 21:44:22 +0100

    first REST API finished

--- c1/handlers/students.js
+++ c1/handlers/students.js
@@ -26,15 +26,47 @@ var addStudent = (req, res) => {
 }
 
 var patchStudent = (req, res) => {
-    res.send('OK');
+    if(data.students[req.params.id] != undefined){
+        if(req.body.ime != undefined){
+            data.students[req.params.id].ime = req.body.ime;
+        }
+        if(req.body.prezime != undefined){
+            data.students[req.params.id].prezime = req.body.prezime;
+        }
+        if(req.body.prosek != undefined){
+            data.students[req.params.id].prosek = req.body.prosek;
+        }
+        return res.status(200).send("OK");
+    } else {
+        return res.status(404).send("Not Found");
+    }
 }
 
 var updateStudent = (req, res) => {
-    res.send('OK');
+    if(data.students[req.params.id] != undefined){
+        if(req.body.ime != undefined && req.body.prezime != undefined && req.body.prosek != undefined){
+            let student = {
+                ime: req.body.ime,
+                prezime: req.body.prezime,
+                prosek: req.body.prosek
+            };
+            data.students[req.params.id] = student;
+            return res.status(200).send("OK");
+        } else {
+            return res.status(400).send("Bad Request");
+        }
+    } else {
+        return res.status(404).send("Not Found");
+    }
 }
 
 var deleteStudent = (req, res) => {
-    res.send('OK');
+    if(data.students[req.params.id] != undefined){
+        data.students.splice(req.params.id, 1);
+        return res.status(204).send("No Content");
+    } else {
+        return res.status(404).send("Not Found");
+    }
 }
 
 
